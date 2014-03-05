fd2dd33ab3457930154a8a3b2c22c93492d12be4
mhrilwan <rilwanmbbsnew123>
2014-03-05 17:17:53 +0530

    MHRUser

--- config/application.config.php
+++ config/application.config.php
@@ -2,12 +2,14 @@
 return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
-        'Application',
-      //  'DoctrineModule',
-      //  'DoctrineORMModule',
+        'ZendDeveloperTools',
+        'DoctrineModule',
+        'DoctrineORMModule',
       //  'AuthDoctrine',
-        'Users',
-        'MHRRest'
+        'Application',
+      //  'Users',
+        'MHRUser'
+      //  'MHRRest'
     ),
 
     // These are various options for the listeners attached to the ModuleManager
--- module/Users/config/module.config.php
+++ module/Users/config/module.config.php
@@ -1,5 +1,23 @@
 <?php
 return array(
+
+
+    'doctrine' => array(
+        'driver' => array(
+            'application_entities' => array(
+                'class' =>'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                'cache' => 'array',
+                'paths' => array(__DIR__ . '/../src/Application/Entity')
+            ),
+
+            'orm_default' => array(
+                'drivers' => array(
+                    'Application\Entity' => 'application_entities'
+                )
+            ))),
+
+
+
     'controllers' => array(
         'invokables' => array(
             'Users\Controller\Index' => 'Users\Controller\IndexController',
